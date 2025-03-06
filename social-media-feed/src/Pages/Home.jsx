57b19fc87c5a2fdb85ea99634f58814b972e83af
Suba-ganesh <subaganesh.p@gmail.com>
2025-03-06 08:40:27 +0530

    sorting feature included

--- social-media-feed/src/Pages/Home.jsx
+++ social-media-feed/src/Pages/Home.jsx
@@ -6,14 +6,39 @@ const axiosInstance = axios.create({ baseURL: "http://localhost:5000" });
 
 
 const Home = () => {
-    const [posts, setPosts] = useState([]);
-    useEffect(() => { axiosInstance.get("/posts").then((res) => setPosts(res.data)); }, []);
-    return (
-      <div>
-        <Navbar />
-        {posts.map((post) => (<PostCard key={post._id} post={post} />))}
-      </div>
-    );
-  };
+  const [posts, setPosts] = useState([]);
+  const [sortBy, setSortBy] = useState("none");
+
+  useEffect(() => {
+    axiosInstance.get("/posts").then((res) => setPosts(res.data));
+  }, []);
+
+  const sortedPosts = [...posts].sort((a, b) => {
+    if (sortBy === "likes") return b.likes - a.likes;
+    if (sortBy === "comments") return (b.comments?.length || 0) - (a.comments?.length || 0);
+    return 0;
+  });
+
+  return (
+    <div>
+      <Navbar />
+      <Box display="flex" justifyContent="center" margin={2}>
+        <Button variant="outlined" onClick={() => setSortBy("likes")} sx={{ mx: 1 }}>
+          Sort by Likes
+        </Button>
+        <Button variant="outlined" onClick={() => setSortBy("comments")} sx={{ mx: 1 }}>
+          Sort by Comments
+        </Button>
+        <Button variant="outlined" onClick={() => setSortBy("none")} sx={{ mx: 1 }}>
+          Clear Sorting
+        </Button>
+      </Box>
+      {sortedPosts.map((post) => (
+        <PostCard key={post._id} post={post} />
+      ))}
+    </div>
+  );
+};
+
 
   export default Home;
